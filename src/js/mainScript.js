9a14d4222502dfcc325f51d89418b698968a96ee
Pavel <pavlik.schavlik@outlook.com>
2022-10-17 22:11:51 +0300

    hw7_cleaneStructure

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -54,11 +54,10 @@ class DrawPokemon {
     async drawPokemonCard(){
         const getMoreData = new PokemonApi(this.url);
         const data = await getMoreData.getInfo();
-        console.log(data)
-        this.drawCard(data)
+        this.renderCard(data);
     }
 
-    drawCard(data){
+    renderCard(data){
         let card = document.createElement('div');
         card.className = 'pokemon';
         document.querySelector('.pokemonCardsContent').append(card);
@@ -71,12 +70,12 @@ class DrawPokemon {
         const spriteBlock = document.createElement('div');
         spriteBlock.className = 'sprite';
         pokemonName.after(spriteBlock);
-        spriteBlock.innerHTML = `<img src="${data.img}">`
+        spriteBlock.innerHTML = `<img src="${data.img}">`;
 
         const statsBlock = document.createElement('div');
         statsBlock.className = 'stats';
         spriteBlock.after(statsBlock);
-        statsBlock.innerHTML = `<span>HP: ${data.stats[0].base_stat}</span><span>ATK: ${data.stats[1].base_stat}</span><span>DEF: ${data.stats[2].base_stat}</span>`
+        statsBlock.innerHTML = `<span>HP: ${data.stats[0].base_stat}</span><span>ATK: ${data.stats[1].base_stat}</span><span>DEF: ${data.stats[2].base_stat}</span>`;
 
         const abilitiesBlock = document.createElement('div');
         abilitiesBlock.className = 'abilities';
@@ -85,15 +84,15 @@ class DrawPokemon {
         const closeButton = document.createElement('button');
         closeButton.className = 'close';
         pokemonName.append(closeButton);
-        closeButton.innerHTML = '<img class="xMark" src="src/xmark-solid.svg">';
-        closeButton.addEventListener('click', () => this.closeCard(card))
+        closeButton.innerHTML = '<img class="xMark" src="src/img/xmark-solid.svg">';
+        closeButton.addEventListener('click', () => this.closeCard(card));
 
         data.ability.forEach(element =>{
             const abilityText = document.createElement('p');
             abilityText.className = 'skill';
             abilitiesBlock.append(abilityText);
-            abilityText.innerHTML = `${element.name}: ${element.effect}`
-        })
+            abilityText.innerHTML = `${element.name}: ${element.effect}`;
+        });
 
     }
 
